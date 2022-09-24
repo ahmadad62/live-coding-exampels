67e39ebc6528ac9dce04477d894061066d9e4a31
ahmadad62 <a.naghian@gmail.com>
2022-09-24 10:53:37 +0200

    first commmit

---
+++ 3_img/index.js
@@ -0,0 +1,5 @@
+const bozos = ["Hosam", "Michael", "Ahmad", "Evgeniia", "Derya", "Klaus", "Lina", "AliBozo", "Etienne", "Mirna", "AhmadJ", "Michelle", "AliR", "Edgar", "Lea", "Niko", "George", "Marvin"];
+
+let result = bozos[Math.floor(Math.random()*bozos.length)];
+
+console.log(result)
